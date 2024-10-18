56f4eb04c61f881f5313409f94411a2084d33c05
Lucas Safeno <lugnorio@gmail.com>
2024-10-18 20:42:16 +0000

    Orgamid - JavaScript EC6+ Iniciante - Variaveis

---
+++ 00-variaveis/exercicio/script.js
@@ -0,0 +1,21 @@
+/**
+ *      Exercicio
+ * 1.Declarar uma variavel com seu nome
+ * 2.Declarar uma variavel com sua idade
+ * 3.Declaar uma variavel com sua comida favorita e não atribuir valor
+ * 4.Atribuir valor a sua comida favorita
+ * 5.Delcrar 5 variaveis diferentes sem calores
+ *
+ */
+
+let nome = "Lucas Tenório";
+console.log(nome);
+let idade = 30;
+let comidaFavorita;
+comidaFavorita = "Parmegiana";
+console.log(comidaFavorita);
+let naoDefini,
+  variavelSemValor,
+  variavelIndefinida,
+  variavelValorUndefined,
+  variavelVazia;
---
+++ 00-variaveis/script.js
@@ -0,0 +1,47 @@
+// var nome = "Lucas";
+// var idade = 30;
+// var possuiFaculdade = true;
+
+// console.log(nome, idade, possuiFaculdade);
+
+// var preco = 25;
+// var totalComprado = 5;
+// var totalPreco = totalComprado * preco;
+
+// console.log(totalPreco);
+
+// //Declarar varivei separadas por virgulas
+// var sobrenome = "Tenório",
+//   cidade = "Recife;";
+
+// console.log(sobrenome, cidade);
+
+// var semDefinir;
+
+// console.log(semDefinir); // undefined
+
+// // Variavel inválidas
+// // var function;
+// // var 1possuiFaculdade;
+
+// // Variaveis válidas
+// var $selecionar;
+// var _nome;
+// var possuiFaculdadeNoExterior;
+
+// // Hoisting
+// var comida;
+// console.log(comida);
+// comida = "Pizza";
+// console.log(comida);
+
+// // Muda valor
+// var time = "Sport";
+// console.log(time);
+// time = "Sport Clube";
+// console.log(time);
+
+// let time = "Nautico";
+// console.log(time);
+// time = "Santa Cruz";
+// console.log(time);
